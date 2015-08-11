14959b71b4a888ee12a78ffdbd38b3b367e1de9d
edigitman <Didubitabillo1>
2015-08-11 23:17:00 +0200

    load knockout and data.json

--- app/app.js
+++ app/app.js
@@ -1,6 +1,14 @@
 var CVModel = function() {
+    var self = this;
+    var cv = null;
 
+    var init = function(){
+        $.getJSON('data.json', function(newResult) {
+            cv = ko.mapping.fromJS(newResult.Data);
 
+        });
+    }
+    init();
 
 };
 
